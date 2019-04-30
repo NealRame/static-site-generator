49e4f2c17dff4e2daf2d396ae1a7832a59b452cd
NealRame <contact@nealrame.com>
2019-04-30 23:59:30 +0200

    Scheme

--- test/site/gulpfile.js
+++ test/site/gulpfile.js
@@ -2,12 +2,22 @@ const CreateGulpTasks = require('../../lib/gulp')
 const {join} = require('path')
 
 module.exports = CreateGulpTasks({
-	content: {
+	'content': {
 		index: true,
 		middlewares: ['blender-menu'],
 	},
-	style: {
+	'style': {
 		sourcesDir: join(__dirname, 'style'),
 		prefix: 'css',
+	},
+	'script:foo': {
+		sourcesDir: join(__dirname, 'applets', 'foo'),
+		name: 'foo',
+		prefix: 'scripts',
+	},
+	'script:bar': {
+		sourcesDir: join(__dirname, 'applets', 'bar'),
+		name: 'bar',
+		prefix: 'scripts',
 	},
 })
