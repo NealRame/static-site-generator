97ef2f3ff305acd0e600bb2908edb87c48994d19
Julien Graziano <julien@graziano.fr>
2020-02-01 00:15:37 +0100

    test: update foo applet

--- tests/site/applets/foo/foo.js
+++ tests/site/applets/foo/foo.js
@@ -1,1 +1,11 @@
-export const foo = () => 'foo applet'
+export const foo = function(n) {
+	let a = 0
+	let b = 1
+	while (n > 0) {
+		const c = a
+		a = b
+		b = c + b
+		n = n - 1
+	}
+	return a
+}
