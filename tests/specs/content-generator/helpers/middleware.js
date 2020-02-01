2841949e4d7144840f7818ad9c1ab282f6fae3fa
Julien Graziano <julien@graziano.fr>
2020-02-01 02:23:43 +0100

    test: update middleware-loader tests

---
+++ tests/specs/content-generator/helpers/middleware.js
@@ -0,0 +1,1 @@
+module.exports = () => files => Promise.resolve()
--- tests/specs/content-generator/middleware/middleware-loader.js
+++ tests/specs/content-generator/middleware/middleware-loader.js
@@ -1,8 +1,8 @@
+const {join} = require('path')
 const test = require('ava')
 const {identical, is, where} = require('ramda')
 
 const State = require('../helpers/state')
-const makeMiddlewareModule = require('../helpers/make-middleware-module')
 const {_MiddlewareLoader} = require('../../../../lib/content-generator/middleware')
 
 test('_MiddlewareLoader should be a function', t => {
@@ -13,25 +13,22 @@ test('_MiddlewareLoader should return a function', t => {
 	t.true(is(Function, _MiddlewareLoader(State())))
 })
 
-test(`Loader should return {builder, config, name} when module 'name' has been found`, async t => {
-	return makeMiddlewareModule({pretend: false}).then(({dir, name}) => {
-		const state = State({middlewaresDirectories: [dir]})
-		const load = _MiddlewareLoader(state)
-		const config = {foo: 'bar'}
-		const middleware = load({name, config})
-		t.true(where({
-			builder: is(Function),
-			config: identical(config),
-			name: identical(name),
-		}, middleware))
-	})
+test('Loader should return {builder, config, name} when module \'name\' has been found', t => {
+	const state = State({middlewaresDirectories: [join(__dirname, '..', 'helpers')]})
+	const load = _MiddlewareLoader(state)
+	const config = {foo: 'bar'}
+	const middleware = load({name: 'middleware', config})
+	t.true(where({
+		builder: is(Function),
+		config: identical(config),
+		name: identical('middleware'),
+	}, middleware))
 })
 
-test(`Loader should throw when module 'name' has not been found`, async t => {
-	return makeMiddlewareModule({pretend: true}).then(({dir, name}) => {
-		const state = State({middlewaresDirectories: [dir]})
-		const load = _MiddlewareLoader(state)
-		const config = {foo: 'bar'}
-		t.throws(() => load({name, config}), {instanceOf: Error})
-	})
+test('Loader should throw when module \'name\' has not been found', t => {
+	const state = State({middlewaresDirectories: []})
+	const load = _MiddlewareLoader(state)
+	const config = {foo: 'bar'}
+	t.throws(() => load({name: 'middleware', config}), {instanceOf: Error})
+
 })
