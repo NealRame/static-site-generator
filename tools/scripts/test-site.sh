549e310bb5e39148dc97c9eb0359a0f6a05fdb9f
Julien Graziano <julien@graziano.fr>
2020-02-01 01:04:20 +0100

    test: set log level to info

--- tools/scripts/test-site.sh
+++ tools/scripts/test-site.sh
@@ -7,7 +7,7 @@ source "$PWD/tools/scripts/.env"
 
 pushd "${SITE_DIR}"
 
-SITE_OUTPUT_BASE_DIR=build gulp $SITE_TASK
-SITE_OUTPUT_BASE_DIR=build-dev SITE_ENV="development" gulp $SITE_TASK
+BLENDER_LOG_LEVEL=info SITE_OUTPUT_BASE_DIR=build gulp $SITE_TASK
+BLENDER_LOG_LEVEL=info SITE_OUTPUT_BASE_DIR=build-dev SITE_ENV="development" gulp $SITE_TASK
 
 popd
