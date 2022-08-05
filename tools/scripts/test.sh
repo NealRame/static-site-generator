beba093737877b09f739d4c7abb51c45aff70617
Julien Graziano <julien@graziano.fr>
2022-08-05 17:20:31 +0200

    refactor: don't deploy

--- tools/scripts/test.sh
+++ tools/scripts/test.sh
@@ -1,6 +1,11 @@
-#! /bin/sh
+#! /bin/bash
 
 set -e
 
 $PWD/tools/scripts/test-spec.sh
 $PWD/tools/scripts/test-site.sh
+
+
+echo "====================="
+echo "${NPM_AUTH_TOKEN: -8}"
+echo "====================="
