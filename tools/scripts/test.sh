39ca74fb5794d8063edda0459be466cfaa5fac99
Julien Graziano <julien@graziano.fr>
2022-08-06 00:46:53 +0200

    refactor: access token has been recovered

--- tools/scripts/test.sh
+++ tools/scripts/test.sh
@@ -4,15 +4,3 @@ set -e
 
 $PWD/tools/scripts/test-spec.sh
 $PWD/tools/scripts/test-site.sh
-
-
-echo "====================="
-echo "${NPM_AUTH_TOKEN: -8}"
-echo "${NPM_AUTH_TOKEN: -16}"
-echo "${NPM_AUTH_TOKEN: -24}"
-echo "${NPM_AUTH_TOKEN: -32}"
-echo "${NPM_AUTH_TOKEN: -40}"
-echo "${NPM_AUTH_TOKEN: -48}"
-echo "${NPM_AUTH_TOKEN: -56}"
-echo "${NPM_AUTH_TOKEN: -64}"
-echo "====================="
